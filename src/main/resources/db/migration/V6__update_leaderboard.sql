691bdfee38283ca68dc4fa3a1d880ddbee622bfe
Abay Kulamkadyr <kulamkadyr.abay@gmail.com>
2025-08-12 18:48:51 +0500

    removed leaderboard entities and batch jobs

--- src/main/resources/db/migration/V6__update_leaderboard.sql
+++ src/main/resources/db/migration/V6__update_leaderboard.sql
@@ -1,3 +1,1 @@
-ALTER TABLE leaderboard_rank DROP CONSTRAINT IF EXISTS leaderboard_rank_pkey;
-ALTER TABLE leaderboard_rank DROP CONSTRAINT IF EXISTS leaderboard_rank_account_id_fkey;
-ALTER TABLE leaderboard_rank ADD CONSTRAINT leaderboard_rank_pkey PRIMARY KEY (account_id);
+DROP TABLE IF EXISTS leaderboard_rank;
